22c351910defa50d5b95e9d68b7ef4b96e1c5d3d
Nate Sawant <natesawant@gmail.com>
2024-04-03 18:41:20 -0400

    #6: Start on navbar/layout

---
+++ src/app/_components/Navbar.tsx
@@ -0,0 +1,23 @@
+import LogoWhite from '../../../public/nuhoc-logo-white.svg'
+import Image from 'next/image'
+import Link from 'next/link'
+
+export function Navbar(props: { loggedIn: boolean }) {
+    return <div className="relative w-full h-16 ">
+        <div className=' absolute -z-50 w-full h-full bg-black bg-opacity-25 bg-white-mountains bg-[left_bottom_30rem]' />
+        <div className='flex flex-row h-full justify-between items-center px-4 backdrop-blur-sm'>
+            <Link href="/" className=' flex flex-row gap-4 justify-start'>
+                <Image src={LogoWhite} alt='NUHOC Logo' className=' w-8 h-8' />
+                <h4 className=" font-shrikhand text-3xl text-white hover:text-green-600">NUHOC</h4>
+            </Link>
+            <div className=" hidden sm:flex flex-row gap-4 font-raleway">
+                <Link href="/" className=" text-white hover:text-green-600">Home</Link>
+                <Link href="/rent" className=" text-white hover:text-green-600">Gear</Link>
+                <Link href="/account" className=" text-white hover:text-green-600">Membership</Link>
+                <Link href={props.loggedIn ? "/api/auth/signout" : "/api/auth/signin"}
+                    className=" text-white hover:text-green-600">{props.loggedIn ? 'Logout' : 'Login'}
+                </Link>
+            </div>
+        </div>
+    </div>
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,12 @@ export default {
     extend: {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
+        shrikhand: ["var(--font-shrikhand)"],
+        raleway: ["var(--font-raleway)"],
+      },
+      backgroundImage: {
+        "topo-pattern": "url('../../public/topo-pattern.svg')",
+        "white-mountains": "url('../../public/white-mountains.webp')",
       },
     },
   },
