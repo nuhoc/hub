1a0bd81889c304f3a8c51a0536a0490d25414131
Nate Sawant <natesawant@gmail.com>
2024-04-27 21:10:58 -0400

    Fix logos in navbar and footer

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -1,12 +1,12 @@
-import Image from 'next/image'
 import Link from 'next/link'
+import NUHOCLogo from './nuhoc-logo';
 
 export function Navbar(props: { loggedIn: boolean }) {
     return <div className="w-full h-16 shrink-0 bg-white-mountains bg-[left_bottom_30rem]">
         <div className='flex flex-row h-full justify-between items-center px-4 backdrop-blur-sm bg-black bg-opacity-25'>
-            <Link href="/" className=' flex flex-row gap-4 justify-start'>
-                <Image src='../../../public/nuhoc-logo-green.svg' width={32} height={32} alt='NUHOC Logo' className=' w-8 h-8' />
-                <h4 className=" font-shrikhand text-3xl text-white hover:text-primary">NUHOC</h4>
+            <Link href="/" className=' flex flex-row gap-4 justify-start fill-white hover:fill-primary text-white hover:text-primary'>
+                <NUHOCLogo className=' w-8 h-8' />
+                <h4 className=" font-shrikhand text-3xl">NUHOC</h4>
             </Link>
             <div className=" hidden sm:flex flex-row gap-4 font-raleway">
                 <Link href="/" className=" text-white hover:text-primary">Home</Link>
